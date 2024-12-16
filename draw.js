d99b755cef5ede80d001a6458936aa7c224f029d
Balearica <admin@scribeocr.com>
2024-12-15 23:52:06 -0800

    Ran linter

--- draw.js
+++ draw.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 import { ScrollView } from './scrollview/ScrollView.js';
 import { writeCanvasNodeAll } from './src/ScrollViewNode.js';
 
@@ -8,13 +8,13 @@ const outputDir = process.argv[3];
 const fileBase = `${outputDir}/${path.basename(filePath)}`;
 
 if (!filePath) {
-  console.error("Please provide a file path as an argument.");
+  console.error('Please provide a file path as an argument.');
   process.exit(1);
 }
 
 const sv = new ScrollView();
 
-const inputData = fs.readFileSync(filePath, { encoding: "utf-8" });
+const inputData = fs.readFileSync(filePath, { encoding: 'utf-8' });
 
 await sv.processVisStr(inputData);
 
